d84a5f2577378673293e87ef74744f6228a37314
annelorainem <almadrid23-0324@cca.edu.ph>
2025-02-17 18:24:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
     <body>
         <h1> student record </h1>
-        <table id ="student_table"> 
+        <table id ="student_table" border=1> 
         </table>
 
         <script>
@@ -23,6 +23,27 @@
 
             row1col1.innerHTML = "LAST NAME";
             row1col2.innerHTML = "FIRST NAME";
+
+            const row2 = studentTable.insertRow();
+            const row2col1 = row2.insertCell();
+            const row2col2 = row2.insertCell();
+
+            const inputLastName = document.createElement("input");
+            inputLastName.type="text";
+            inputLastName.placeholder="insert Last Name";
+
+            row2col1.append(inputLastName);
+
+            const inputFirstName = document.createElement("input");
+            inputFirstName.type="text";
+            inputFirstName.placeholder="insert First Name";
+
+            row2col2.append(inputFirstName);
+
+            const row3 = studentTable.insertRow();
+            const row3col1 = row3.insertCell();
+            row3col1.colSpan="2";
+            row3col1.innerHTML = "TEST";
         </script>
     </body>
 
