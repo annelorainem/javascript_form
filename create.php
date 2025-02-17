f41d50939667c631611000894dbd6747ec61cf82
annelorainem <almadrid23-0324@cca.edu.ph>
2025-02-17 19:41:22 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,8 +1,13 @@
 <?php
     echo"Hello World";
 
-    var_dump ($_POST);
-    
+    var_dump($_POST);
+    $lastname = $_POST['Last_name'];
+    $firstname = $_POST['First_name'];
+?>
+    <br>
+    <br>
+ <?php   
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -14,6 +19,19 @@
 // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
+        }
+        echo "Connected successfully";
+
+    $sql = "INSERT INTO students (id, last_name, first_name)
+    VALUES ('', '$lastname', '$firstname')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+
+        echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
 ?>
