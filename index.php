e40378bfd4c03487d43b6165776645759eb5f46a
annelorainem <almadrid23-0324@cca.edu.ph>
2025-02-17 17:51:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>javascript form</title>
+    
+    </head> 
+
+    <body>
+        <h1> student record </h1>
+        <table id =<"student_table"> 
+        </table>
+
+        <script>
+            const studentTable = document
+                .getElementById("student_table") ;
+
+            console.log(studentTable);
+        </script>
+    </body>
+
+</html>
