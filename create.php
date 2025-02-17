759550e3c6c2f622d79d44967491e6b8ded36a7c
annelorainem <almadrid23-0324@cca.edu.ph>
2025-02-17 19:22:18 +0800

    Update create.php

--- create.php
+++ create.php
@@ -3,4 +3,17 @@
 
     var_dump ($_POST);
     
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "loraine";
+
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
 ?>
